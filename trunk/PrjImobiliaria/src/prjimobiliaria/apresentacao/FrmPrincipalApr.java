e7fc5cdf24dd4663a93b1df69784187b7ac1e136
sandroware7 <sandroware7@3a283111-f058-e3e6-0066-93c49b6401fc>
2010-05-12 00:59:16 +0000

    Remoção do menu Consultas da TelaPrincipal

--- trunk/PrjImobiliaria/src/prjimobiliaria/apresentacao/FrmPrincipalApr.java
+++ trunk/PrjImobiliaria/src/prjimobiliaria/apresentacao/FrmPrincipalApr.java
@@ -35,7 +35,6 @@ public class FrmPrincipalApr extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
 
@@ -56,9 +55,6 @@ public class FrmPrincipalApr extends javax.swing.JFrame {
         jMenu1.add(jMenuItem2);
 
         jMenuBar1.add(jMenu1);
-
-        jMenu2.setText("Consultas");
-        jMenuBar1.add(jMenu2);
 
         jMenu3.setText("Relatórios");
         jMenuBar1.add(jMenu3);
@@ -112,7 +108,6 @@ public class FrmPrincipalApr extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
