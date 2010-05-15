99adc829951625bd96ed1770b63403389e1f364b
sandroware7 <sandroware7@3a283111-f058-e3e6-0066-93c49b6401fc>
2010-05-15 00:40:05 +0000

    Correções

--- trunk/PrjImobiliaria/src/prjimobiliaria/dao/UsuarioDao.java
+++ trunk/PrjImobiliaria/src/prjimobiliaria/dao/UsuarioDao.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package prjimobiliaria.dao;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import prjimobiliaria.negocio.*;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -20,19 +17,33 @@ public class UsuarioDao {
      * Método que insere novo
      * @return int
      */
-    public int inserirUsuario(Usuario usr) {
+    public int inserirUsuario(Usuario usr) throws Exception {
 
+        ConexaoDao conDao = null;
+        Connection con = null;
+        PreparedStatement ps = null;
         int result = 0;
-
+        
         try {
 
-            String sql = "INSERT INTO tbusuario VALUES ('"+usr.getDsLogin()+"','"+ usr.getDsLogin()+"',"+usr.getIdPessoa()+")";
+            String sql = "INSERT INTO TbUsuario VALUES (?,?,?)";
 
-            ConexaoDao con = new ConexaoDao();
-            result = con.executarCud(sql);
+            conDao = new ConexaoDao();
+            con = conDao.abrirConexao();
+            
+            ps = con.prepareStatement(sql);
+            
+            ps.setString(1,usr.getDsLogin());
+            ps.setString(2,usr.getDsSenha());
+            ps.setInt(3,usr.getIdPessoa());
+
+            result = ps.executeUpdate();
 
         } catch (Exception ex) {
              ex.printStackTrace();
+        } finally {
+
+            conDao.fecharConexao();
         }
 
         return result;
@@ -43,23 +54,32 @@ public class UsuarioDao {
      * Método que atualiza um usuário, específicado pelo id
      * @return int
      */
-    public int atualizarUsuario(Usuario usr) {
+    public int atualizarUsuario(Usuario usr) throws Exception {
 
+        ConexaoDao conDao = null;
+        Connection con = null;
+        PreparedStatement ps = null;
         int result = 0;
 
         try {
-            
-            String sql = "UPDATE tbusuario SET " +
-                               "dsLogin = '"+ usr.getDsLogin()+"','"+
-                               "dsSenha = '"+ usr.getDsSenha()+"' "+
-                         "WHERE " +
-                               "idPessoa = "+usr.getIdPessoa();
-            
-            ConexaoDao con = new ConexaoDao();
-            result = con.executarCud(sql);
 
+            String sql = "UPDATE tbusuario SET dsLogin = ?, dsSenha = ? WHERE idPessoa = ?";
+
+            conDao = new ConexaoDao();
+            con = conDao.abrirConexao();
+
+            ps = con.prepareStatement(sql);
+
+            ps.setString(1,usr.getDsLogin());
+            ps.setString(2,usr.getDsSenha());
+            ps.setInt(3,usr.getIdPessoa());
+
+            result = ps.executeUpdate();
+ 
         } catch (Exception ex) {
              ex.printStackTrace();
+        } finally {
+            conDao.fecharConexao();
         }
 
         return result;
@@ -70,21 +90,33 @@ public class UsuarioDao {
      * Método que exclui um usuário, especificado pelo id
      * @return int
      */
-    public int excluirUsuario(Usuario usr) {
+    public int excluirUsuario(Usuario usr) throws Exception {
 
+        ConexaoDao conDao = null;
+        Connection con = null;
+        PreparedStatement ps = null;
         int result = 0;
 
         try {
 
-            String sql = "DELET FROM tbusuario WHERE idPessoa = "+usr.getIdPessoa();
+            String sql = "DELET FROM tbusuario WHERE idPessoa = ?";
 
-            ConexaoDao con = new ConexaoDao();
-            result = con.executarCud(sql);
+            conDao = new ConexaoDao();
+            con = conDao.abrirConexao();
+
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1,usr.getIdPessoa());
+
+            result = ps.executeUpdate();
 
         } catch (Exception ex) {
              ex.printStackTrace();
-        }
+        } finally {
 
+            conDao.fecharConexao();
+        }
+       
         return result;
     }
 
@@ -93,23 +125,33 @@ public class UsuarioDao {
      * Método que retorna todos os usuários cadastrados
      * @return ResultSet
      */
-    public ResultSet obterUsuario() throws SQLException {
+    public ResultSet obterUsuario() throws SQLException, Exception {
 
+        ConexaoDao conDao = null;
+        Connection con = null;
+        PreparedStatement ps = null;
         ResultSet result = null;
 
         try {
 
-            String sql = "SELECT * " +
-                         "FROM " +
+             String sql = "SELECT * " +
+                          "FROM " +
                               "tbusuario u " +
-                         "INNE JOIN tbpessoa p ON " +
+                          "INNE JOIN tbpessoa p ON " +
                               "u.idPessoa = p.idPessoa";
-                         
-            ConexaoDao con = new ConexaoDao();
-            result = con.executarRead(sql);
+
+            conDao = new ConexaoDao();
+            con = conDao.abrirConexao();
+
+            ps = con.prepareStatement(sql);
+
+            result = ps.executeQuery();
 
         } catch (Exception ex) {
              ex.printStackTrace();
+        } finally {
+
+            conDao.fecharConexao();
         }
 
         return result;
