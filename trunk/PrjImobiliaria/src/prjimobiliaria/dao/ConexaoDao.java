3eb7a82fb961a8e8acf3f01f9bc6445d05c58722
sandroware7 <sandroware7@3a283111-f058-e3e6-0066-93c49b6401fc>
2010-05-15 00:40:15 +0000

    Correções

--- trunk/PrjImobiliaria/src/prjimobiliaria/dao/ConexaoDao.java
+++ trunk/PrjImobiliaria/src/prjimobiliaria/dao/ConexaoDao.java
@@ -11,37 +11,13 @@ import java.sql.SQLException;
  * @author Elissandro
  */
 public class ConexaoDao {
-    
-    private Connection _conexao;
-    
-    
+
+    Connection _conexao;
+
     // Construtor
     public ConexaoDao() {
 
-        try {
-
-            _conexao = abrirConexao();
-
-        } catch (Exception ex) {
-
-            ex.printStackTrace();
-        }
     }
-
-    /**
-     * @param conexao the _conexao to set
-     */
-    public void setConexao(Connection conexao) {
-        this._conexao = conexao;
-    }
-
-    /**
-     * @return the _conexao
-     */
-    public Connection getConexao() {
-        return _conexao;
-    }
-
 
     //Método que realiza e retorna conexão com o banco
     public Connection abrirConexao() throws Exception {
@@ -50,7 +26,7 @@ public class ConexaoDao {
 
             Class.forName ("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/dbimobiliaria";
-            setConexao(DriverManager.getConnection(url, "sysdba", "masterkey"));
+            _conexao = DriverManager.getConnection(url, "sysdba", "masterkey");
         }
         catch(ClassNotFoundException ex) {
 
@@ -61,7 +37,7 @@ public class ConexaoDao {
             throw new Exception("[ERRO_ABRIR_CONEXAO] " + ex.getMessage());
         }
 
-        return getConexao();
+        return _conexao;
     }
 
     //Método que fecha conexão com o banco
@@ -69,7 +45,7 @@ public class ConexaoDao {
 
         try {
 
-            getConexao().close();
+            _conexao.close();
 
         } catch (SQLException ex) {
 
@@ -77,48 +53,4 @@ public class ConexaoDao {
         }
     }
     
-    //Método de Persistência para Create(Insert), Update e Delete
-    public int executarCud(String sql) throws Exception {
-        
-        int result = 0;
-        
-        try {
-            
-            Statement st = getConexao().createStatement();
-            result = st.executeUpdate(sql);
-        
-        } catch (SQLException ex) {
-        
-            ex.printStackTrace();
-            
-        } finally {
-        
-            fecharConexao();
-        }
-
-        return result;
-    }
-    
-    //Método de Persistência para Read(Select)
-    public ResultSet executarRead(String sql) throws Exception {
-
-        ResultSet result = null;
-        
-        try {
-
-            Statement st = getConexao().createStatement();
-            result = st.executeQuery(sql);
-            
-        } catch (SQLException ex) {
-            
-            ex.printStackTrace();
-            
-        } finally {
-            
-            fecharConexao();
-        }
-
-        return result;
-    }
-
 }
