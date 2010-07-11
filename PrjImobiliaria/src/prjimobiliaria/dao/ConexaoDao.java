f95339420aaa8d571b2d5f1c4552c5f0fef344e5
alexandre.pereiramenezes <alexandre.pereiramenezes@3a283111-f058-e3e6-0066-93c49b6401fc>
2010-07-11 14:17:33 +0000

    alteração em cliente e usuario

--- PrjImobiliaria/src/prjimobiliaria/dao/ConexaoDao.java
+++ PrjImobiliaria/src/prjimobiliaria/dao/ConexaoDao.java
@@ -21,18 +21,18 @@ public class ConexaoDao {
     public Connection abrirConexao() throws Exception {
 
         try {
-
             Class.forName ("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/db_imobiliaria";
-            _conexao = DriverManager.getConnection(url, "sysdba", "masterkey");
+            _conexao = DriverManager.getConnection(url, "postgres", "12345");
+           
         }
         catch(ClassNotFoundException ex) {
 
             throw new Exception("[ERRO_DRIVER] Driver JDBC-ODBC não encontrado! " + ex.getMessage());
         }
         catch(SQLException ex) {
-
-            throw new Exception("[ERRO_ABRIR_CONEXAO] " + ex.getMessage());
+           
+            throw new Exception("[ERRO_ABRIR_CONEXAO] " );
         }
 
         return _conexao;
